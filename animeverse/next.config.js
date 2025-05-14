e8e5b18950f8d044aabe3b77f21164a292656a71
AmadeusLimMC <amadeus.socoe@gmail.com>
2025-05-15 00:45:12 +0800

    fix export base path prefixing

---
+++ animeverse/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+
+	output: 'export',
+	basePath: '/animeverse',
+	assetPrefix: '/animeverse/',
+	trailingSlash: true,
+
+}
+
+module.exports = nextConfig
